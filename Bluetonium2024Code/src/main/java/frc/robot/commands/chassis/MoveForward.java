474a70db00de7d58aa26fb5683b0ababe16c25a6
ok yay <jesuschristisafalicy@gmail.com>
2024-03-01 13:07:04 -0600

    make auton do?
    idk

--- Bluetonium2024Code/src/main/java/frc/robot/commands/chassis/MoveForward.java
+++ Bluetonium2024Code/src/main/java/frc/robot/commands/chassis/MoveForward.java
@@ -7,30 +7,26 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Swerve;
 
 public class MoveForward extends Command {
-    private int timer;
+    private long milliStart;
     private Swerve swerve;
 
     public MoveForward(Swerve swerve) {
         addRequirements(swerve);
-        timer = 100;
+        milliStart = System.currentTimeMillis();
         this.swerve = swerve;
     }
-
     @Override
     public void execute() {
         SwerveModuleState[] states = new SwerveModuleState[4];
         for (SwerveModuleState state : states) {
             state.angle = new Rotation2d(0, 0);
-            if (timer > 0) {
-                timer--;
+            if (Math.abs(System.currentTimeMillis()-milliStart)<2000) { //i use abs just in case
                 state.speedMetersPerSecond = 1;
             } else {
                 state.speedMetersPerSecond = 0;
             }
         }
-
         swerve.setModuleStates(states, true);
 
     }
-
 }
