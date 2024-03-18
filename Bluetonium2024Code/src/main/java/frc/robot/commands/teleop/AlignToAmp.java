0f51a57b95d46e4130bae7229e7d249cdf69a589
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2024-03-18 16:13:04 -0500

    formating

---
+++ Bluetonium2024Code/src/main/java/frc/robot/commands/teleop/AlignToAmp.java
@@ -0,0 +1,13 @@
+package frc.robot.commands.teleop;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+/*//TODO implement this
+ * How it willl work
+ * use limelight to detect amp april tag, if detected move drive train till its centered
+ * find a way to also make sure it facing the amp directly, maybe through 3d tracking
+ * allow the driver to move forward and backward to score amp
+ */
+public class AlignToAmp extends Command {
+
+}
