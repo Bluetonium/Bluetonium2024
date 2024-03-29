a1338cf2583b988e06ecf3281e080d8d9228f5e5
ok yay <jesuschristisafalicy@gmail.com>
2024-03-29 15:36:04 -0500

    i forgor

--- Bluetonium2024Code/src/main/java/frc/robot/commands/auton/speakershoot/PrepareToShoot.java
+++ Bluetonium2024Code/src/main/java/frc/robot/commands/auton/speakershoot/PrepareToShoot.java
@@ -7,7 +7,7 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Swerve;
 
 public class PrepareToShoot extends ParallelCommandGroup {
-    public PrepareToShoot(Swerve swerve, Arm arm, Shooter shooter, NetworkTable limelight) {
+    public PrepareToShoot(Swerve swerve, Arm arm, Shooter shooter) {
         addCommands(new AimAtSpeaker(arm),
                 new RotateToSpeaker(swerve),
                 new SpinUpShooter(shooter));
